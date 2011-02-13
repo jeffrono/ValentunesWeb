38b18cc752bfdb4fd9e2846f86edd02cca3547ac
unknown <Jeffro@.(none)>
2011-02-13 02:58:12 -0500

    added more to song selector php, listing tracks better

--- index.php
+++ index.php
@@ -1,52 +1,11 @@
 <html>
 <head>
 <title>
-  Valentun.es | Send love with music (passionate hackers?)
+  Valentun.es | Send love with music
 </title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <script src="http://cachedcommons.org/cache/jquery/1.4.2/javascripts/jquery-min.js"></script>
-    <script src="http://cachedcommons.org/cache/jquery-ui/1.8.4/javascripts/jquery-ui-min.js"></script>
-    <script>
-      function ri(min, max){
-        return Math.floor(Math.random()* (max - min) + min)
-      }
-
-      function insertPhotosFrom(tags, position){
-        var tag = tags[ri(0, tags.length)];
-        $("#images").attr("class", tag);
-        $.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=79d25c376a49d0d16eb0b25bb8c23dd5&tags="+tag+"&tag_mode=all&sort=interestingness-desc&license=1%2C2%2C3%2C4%2C5%2C6%2C7&content_type=1&extras=url_m%2Cdescription%2Cowner_name%2Clicense&format=json&jsoncallback=?", function(data){
-          var photos = data.photos.photo;
-          insertRandom(photos,tag, position);
-        });
-      }
-
-      function insertRandom(photos, tag, position){
-
-          var max = photos.length;
-          
-            var item = photos.splice(ri(0,max),1)[0];
-          //$.each(data.photos.photo, function(i,item){
-            var photoUrl = item.url_m;
-            //photo page urls = http://www.flickr.com/photos/{user-id}/{photo-id}
-            var photohref = 'http://www.flickr.com/photos/'+item.owner+'/'+item.id;
-            var degree = ri(-10,10);
-            
-            $('<img/>').attr("src", photoUrl).attr("id", "img"+position).appendTo("#images")
-            .wrap("<div class='photo d"+ degree + "d "+tag+"' id='pic"+position+"'></div>").wrap("<a target='_blank' href='" + photohref + "'></a>");
-      }
-      
-      $(document).ready(function(){
-        //var tags = ["love","happy","valentine"];
-        //insertPhotosFrom(["valentine"],1);
-        //insertPhotosFrom(["passion"],1);
-        //insertPhotosFrom(["beautiful"],2);
-        //insertPhotosFrom(["son", "surveillance", "camera"],3);
-        //insertPhotosFrom(["repetition","recursion"],4);
-        $("#license").draggable({helper: "clone"});
-      });
-    </script>
-
-
+    
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
 </html>
@@ -57,9 +16,8 @@
   <p>Create a customized playlist of music your Valentine will love...</p>
 </div>
 
-
 <div id="content">
-<form method="POST" action="process_request.php">
+<form method="POST" action="song_selector.php">
 	<div>
 		 <label for="name">Recipient's Name</label><br>
 		 <input style="content" type="text" name="name" id="name" size="50" value="madeleine" tabindex="1" />
--- song_selector.php
+++ song_selector.php
@@ -4,7 +4,7 @@ session_start();
 <html>
 <head>
 <title>
-  Valentun.es | Passionate hackers
+	Valentun.es | Send love with music
 </title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 <script src="js/polling.js"></script>
@@ -14,29 +14,44 @@ session_start();
 </head>
 </html>
 <body>
+
 <?php 
+/*
 include_once 'connectors/RemoteLogic.php';
+
 // just keep it 
 $_SESSION['valentunes']['POST'] = $_POST;
 
 // send info to backend
 $remote = new RemoteLogic();
 $ticket = $remote->create($_POST['name'], $_POST['interests'], $_POST['description']);
+
+// ** this is throwing error - adding session up top did not fix... what to do?
 //$_SESSION['ticket']; // store session
 
 print '<script type="text/javascript">var job_id = ' . $ticket . '</script>';
+*/
 ?>
 
-
 <div id="header">
-  <img src="" alt="logo" />
-  <p>Create a personalized valentunes gift for your beloved...</p>
+  <img src="./imgs/logo.png" alt="logo" />
+  <p>Almost there.  Please select some songs to include in your ValenTUNE:</p>
 </div>
 
-
+<form method="POST" action="complete.php">
 <div id="content">
- <img src="imgs/alove.gif" />
+	<ul>
+		<li><span style="font: MyUnderwoodRegular; font-size: 30px;"><input type="checkbox" name="track" value="idnum"><a href="test">Artist - Track</a></span>
+		<li><span style="font: MyUnderwoodRegular; font-size: 30px;"><input type="checkbox" name="track" value="idnum"><a href="test">Artist - Track</a></span>
+		<li><span style="font: MyUnderwoodRegular; font-size: 30px;"><input type="checkbox" name="track" value="idnum"><a href="test">Artist - Track</a></span>
+		<li><span style="font: MyUnderwoodRegular; font-size: 30px;"><input type="checkbox" name="track" value="idnum"><a href="test">Artist - Track</a></span>
+		<li><span style="font: MyUnderwoodRegular; font-size: 30px;"><input type="checkbox" name="track" value="idnum"><a href="test">Artist - Track</a></span>
+		<li><span style="font: MyUnderwoodRegular; font-size: 30px;"><input type="checkbox" name="track" value="idnum"><a href="test">Artist - Track</a></span>
+	<input type="submit" name="get_url" value="URL Link" class="btn" />
+	<input type="submit" name="phone_call" value="Phone Call" class="btn" />
 </div>
+</form>
+
 
 </body>
 </html>
